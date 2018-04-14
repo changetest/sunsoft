ea8e437fc89da07f42a398b9d7a0f7f8948c4b3c
kk357817697 <357817697@qq.com>
2018-04-14 17:37:00 +0800

    this testing commit to dev222222

---
+++ src/cmo/csc/TestCommit.java
@@ -0,0 +1,10 @@
+package cmo.csc;
+
+/**
+ * Created by Administrator on 2018/4/14.
+ */
+public class TestCommit {
+    public static void main(String[] args) {
+        System.out.println("test commit to dev");
+    }
+}
